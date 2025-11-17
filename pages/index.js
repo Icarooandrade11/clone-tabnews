42e0bc0ef2752bb08eedd638ea003668e8e61e78
Ícaro Andrade <icarodeasantos@gmail.com>
2025-11-17 01:12:58 +0000

    Desafios

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 function Home() {
-    return <h1> Você aí... Sim, você mesmo! Você consegue tudo, creia!!
-                Agora desce e paga 10 Flexões, campeão! 💪
+    return (
+    <div>
 
-            </h1>
+        <h1> Você aí... Sim, você mesmo! Você consegue tudo, creia!! </h1>
+            
+        <p>
+        O servo do Senhor <br />
+        1“Eis o meu servo, a quem sustento;
+        o meu escolhido, em quem tenho prazer.
+        Porei sobre ele o meu Espírito,
+        e ele trará justiça às nações.
+        </p>
 
+        <p>
+        2Não gritará, nem clamará,
+         nem erguerá a sua voz nas ruas. <br />
+        3Não quebrará o caniço ferido
+         nem apagará o pavio fumegante.
+         Com fidelidade, fará justiça;
+        </p>
+
+        <p>
+        4não mostrará fraqueza nem se deixará ferir,
+         até que estabeleça a justiça na terra. <br />
+        Na lei dele as ilhas porão a sua esperança.”
+        </p>
+
+        <h1> Agora desce e paga 10 Flexões, campeão! 💪 </h1>
+
+   </div>
+    );
 } 
 
 export default Home;
