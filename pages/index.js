51a6856d1d2f73970d94604771baf5952b2b28a1
Ícaro Andrade <icarodeasantos@gmail.com>
2025-11-17 00:33:48 +0000

    Acrescentando desafios

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() {
-    return <h1> Você aí... Sim, você mesmo! Você consegue tudo, creia!!</h1>
-    
+    return <h1> Você aí... Sim, você mesmo! Você consegue tudo, creia!!
+                Agora desce e paga 10 Flexões, campeão! 💪
+
+            </h1>
+
 } 
 
 export default Home;
